232928563493ca34282483d9fc2703cc32c429c3
Carles Núñez Tomeo <carles.nunez@scmspain.com>
2018-03-08 14:24:35 +0100

    feat(tests): Fixed tests to work with faketimers instead of work with native timers

--- test/FunctionThrottlerSpec.js
+++ test/FunctionThrottlerSpec.js
@@ -11,22 +11,27 @@ describe('FunctionThrottler', () => {
     throttledFunction = FunctionThrottler.throttle(callback, 200)
   })
 
-  it('should call the callback after a execution gap of 200 MS between calls if first was executed', (done) => {
+  it('should call the callback at first execution instantly', () => {
+    let clock = sinon.useFakeTimers()
     throttledFunction()
+    expect(callback.callCount).to.be.equal(1)
+    clock.tick(500)
+  })
 
-    setTimeout(() => {
-      expect(callback.callCount).to.be.equal(1)
-    }, 200)
-
+  it('should preserve the gap by configuration 200MS between calls', () => {
+    let clock = sinon.useFakeTimers()
+    callback.resetHistory()
+    clock.tick(100)
     setTimeout(() => {
       throttledFunction()
-      expect(callback.callCount).to.be.equal(1)
     }, 150)
-
+    setTimeout(() => {
+      throttledFunction()
+    }, 160)
     setTimeout(() => {
       throttledFunction()
       expect(callback.callCount).to.be.equal(2)
-      done()
     }, 200)
+    clock.tick(100)
   })
 })
